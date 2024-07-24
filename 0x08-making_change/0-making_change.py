ed56b3a31c92d3c1ce62cfc3259c20202ea1e678
sara <saramustafa1842002@gmail.com>
2024-07-24 04:57:15 +0200

    done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,14 +11,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
     check = 0
-     temp = 0
-                                                        coins.sort(reverse=True)
-                                                            for i in coins:
-                                                                        while check < total: 
-                                                                                        check += i
-                                                                                                    temp += 1
-                                                                                                            if check == total:
-                                                                                                                            return temp
-                                                                                                                                check -= i
-                                                                                                                                        temp -= 1
-                                                                                                                                            return -1
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total: 
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
